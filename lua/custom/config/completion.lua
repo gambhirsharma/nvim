f7afd8c2195f4a9f9158b2fb4e761ced663e5c76
Gambhir ⚡ <gambhir786191ss@gmail.com>
2024-07-02 03:46:47 +0530

    fix: luasnip snippet dir

--- lua/custom/config/completion.lua
+++ lua/custom/config/completion.lua
@@ -18,6 +18,8 @@ luasnip.config.set_config {
   updateevents = "TextChanged, TextChangedI",
   enable_autosnippets = true,
 }
+require("luasnip.loaders.from_lua").load({ paths = "~/.config/kickstart/snippets" })
+
 
 require('lspsaga').setup({})
 
