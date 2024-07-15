7f22b2e7be7dd4bd4459558acb5b5382728b225b
Gambhir ⚡ <gambhir786191ss@gmail.com>
2024-07-16 04:25:18 +0530

    fix: indent_line

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -1,9 +1,28 @@
-return {
-  { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    opts = {},
+return
+{
+  "lukas-reineke/indent-blankline.nvim",
+  -- event = "LazyFile",
+  opts = {
+    indent = {
+      char = "│",
+      tab_char = "│",
+    },
+    scope = { show_start = false, show_end = false },
+    exclude = {
+      filetypes = {
+        "help",
+        "alpha",
+        "dashboard",
+        "neo-tree",
+        "Trouble",
+        "trouble",
+        "lazy",
+        "mason",
+        "notify",
+        "toggleterm",
+        "lazyterm",
+      },
+    },
   },
+  main = "ibl",
 }
