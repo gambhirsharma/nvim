ced09bece34a336d8700ce64e54f956b64e618ec
Gambhir ⚡ <gambhir786191ss@gmail.com>
2024-07-22 04:38:34 +0530

    fix: python workspace

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -149,8 +149,8 @@ return {
         elixirls = {},
         -- clangd = {},
         -- gopls = {},
-          yamlls = {},
-        -- pyright = {},
+        yamlls = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
