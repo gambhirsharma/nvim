05bf5b6837d0716db8e4f9c6eabd9f8f04b96476
Gambhir ⚡ <gambhir786191ss@gmail.com>
2024-11-19 23:10:51 +0100

    update: Obsidian vault

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -3,18 +3,19 @@ return
   "epwalsh/obsidian.nvim",
   lazy = true,
   version = "*", -- recommended, use latest release instead of latest commit
-  -- ft = "markdown",
+  ft = "markdown",
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  event = {
+  -- event = {
     --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
     --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-    "BufReadPre /Users/gambhirsharma/Documents/OBSIDIAN/**.md",
-    "BufNewFile /Users/gambhirsharma/Documents/OBSIDIAN/**.md",
-  },
+  --   "BufReadPre /Users/gambhirsharma/Documents/OBSIDIAN/**.md",
+  --   "BufNewFile /Users/gambhirsharma/Documents/OBSIDIAN/**.md",
+  --   "BufNewFile /Users/gambhirsharma/Library/CloudStorage/OneDrive-Universita'degliStudidiRomaTorVergata/Engineering-Sciences/**.md",
+  -- },
   dependencies = {
     -- Required.
     "nvim-lua/plenary.nvim",
-      lazy= true,
+    lazy = true,
 
     -- see below for full list of optional dependencies 👇
   },
@@ -24,9 +25,14 @@ return
         name = "OBSIDIAN",
         path = "/Users/gambhirsharma/Documents/OBSIDIAN",
       },
+      -- {
+      --   name = "twitter-content",
+      --   path = "/users/gambhirsharma/Documents/Twitter-content",
+      -- },
       {
-        name = "twitter-content",
-        path = "/users/gambhirsharma/Documents/Twitter-content",
+        name = "Engineering-Sciences",
+        path =
+        "/Users/gambhirsharma/Library/CloudStorage/OneDrive-Universita'degliStudidiRomaTorVergata/Engineering-Sciences"
       },
     },
     -- see below for full list of options 👇
