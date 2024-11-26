b22dfc8e41341dd5a38be9f66254a0d6e66d31a4
Gambhir ⚡ <gambhir786191ss@gmail.com>
2024-11-26 19:21:11 +0100

    update: autocmd

--- plugin/autocmd.lua
+++ plugin/autocmd.lua
@@ -5,3 +5,24 @@
 --         vim.cmd("source ../after/ftplugin/javascript.lua")
 --       end
 -- })
+-- vim.api.nvim_create_user_command('MyAvanteChat', function()
+--   require("yetone/avante.nvim").open({
+--     windows = {
+--       width = 100,
+--     },
+--   })
+-- end, {})
+--
+function _G.compare_to_clipboard()
+	local ftype = vim.api.nvim_eval("&filetype")
+	vim.cmd("vsplit")
+	vim.cmd("enew")
+	vim.cmd("normal! P")
+	vim.cmd("setlocal buftype=nowrite")
+	vim.cmd("set filetype="..ftype)
+	vim.cmd("diffthis")
+	vim.cmd([[execute "normal! \<C-w>h"]])
+	vim.cmd("diffthis")
+end
+
+vim.keymap.set("n", "<leader>cc", _G.compare_to_clipboard, { desc = "Compare with clipboard", noremap = true, silent = true })
