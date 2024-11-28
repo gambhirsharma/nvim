62b836dc3b77beaf6a9745e846ac7488f29e9138
Gambhir ⚡ <gambhir786191ss@gmail.com>
2024-11-28 12:36:15 +0100

    fix: luasnip keymap

--- lua/custom/config/completion.lua
+++ lua/custom/config/completion.lua
@@ -108,12 +108,12 @@ cmp.setup {
     -- <c-h> is similar, except moving you backwards.
     -- ['<tab>']
     --
-    ['<C-n>'] = cmp.mapping(function()
+    ['<Tab>'] = cmp.mapping(function()
       if luasnip.expand_or_locally_jumpable() then
         luasnip.expand_or_jump()
       end
     end, { 'i', 's' }),
-    ['<C-p>'] = cmp.mapping(function()
+    ['<S-Tab>'] = cmp.mapping(function()
       if luasnip.locally_jumpable(-1) then
         luasnip.jump(-1)
       end
