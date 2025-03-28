5778f77b5904c501fd1fe755e0005ad29cef0b87
Gambhir ⚡ <gambhir786191ss@gmail.com>
2025-03-28 13:49:08 +0100

    snip: Markdown code block

--- snippets/markdown.lua
+++ snippets/markdown.lua
@@ -3,4 +3,15 @@ return {
   s("sub", fmt([[<sub>{}</sub>]], { i(1) })),
   s("sup", fmt([[<sup>{}</sup>]], { i(1) })),
   s("sqr", fmta([[\sqrt{<>}]], { i(1) })),
+  s("bash", fmta([[```bash 
+<>
+```]], { i(1) })),
+  s("html", fmta([[```html
+<>
+```]], {i(1)})),
+    s("details", fmt([[<details>
+    <summary>{}</summary>
+    {}
+</details>]], {i(1), i(0)}))
+
 }
