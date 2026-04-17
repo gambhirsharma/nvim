79d4a00e21550f867861c94f550c033d89596ca0
Gambhir ⚡ <gambhir786191ss@gmail.com>
2026-04-17 15:13:26 +0200

    chore(plug): disable avante

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -1,73 +1,74 @@
-return {
-  -- FIX: Make the plugin lazy load
-  --
-  "yetone/avante.nvim",
-  event = "VeryLazy",
-  lazy = true,
-  version = false, -- set this if you want to always pull the latest change
-  -- commit = "868c136",
-  --
-  opts = {
-   provider = "copilot",
-    windows = {
-      -- position = "left",
-      input = {
-        height = 8,
-      }
-    },
-      -- MCP Hub Setup
-      system_prompt = function()
-        local hub = require("mcphub").get_hub_instance()
-        return hub:get_active_servers_prompt()
-      end,
-      -- The custom_tools type supports both a list and a function that returns a list. Using a function here prevents requiring mcphub before it's loaded
-      custom_tools = function()
-        return {
-          require("mcphub.extensions.avante").mcp_tool(),
-        }
-      end,
-
-  },
-
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  build = "make",
-
-  -- config = function()
-  --   require("avante").setup({})
-  -- end,
-
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "stevearc/dressing.nvim",
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    --- The below dependencies are optional,
-    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    "zbirenbaum/copilot.lua", -- for providers='copilot'
-    {
-      -- support for image pasting
-      "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
-      opts = {
-        -- recommended settings
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-          -- required for Windows users
-          use_absolute_path = true,
-        },
-      },
-    },
-    {
-      -- Make sure to set this up properly if you have lazy=true
-      'MeanderingProgrammer/render-markdown.nvim',
-      opts = {
-        file_types = { "markdown", "Avante" },
-      },
-      ft = { "markdown", "Avante" },
-    },
-  },
-}
+return {}
+-- return {
+--   -- FIX: Make the plugin lazy load
+--   --
+--   "yetone/avante.nvim",
+--   event = "VeryLazy",
+--   lazy = true,
+--   version = false, -- set this if you want to always pull the latest change
+--   -- commit = "868c136",
+--   --
+--   opts = {
+--    provider = "copilot",
+--     windows = {
+--       -- position = "left",
+--       input = {
+--         height = 8,
+--       }
+--     },
+--       -- MCP Hub Setup
+--       system_prompt = function()
+--         local hub = require("mcphub").get_hub_instance()
+--         return hub:get_active_servers_prompt()
+--       end,
+--       -- The custom_tools type supports both a list and a function that returns a list. Using a function here prevents requiring mcphub before it's loaded
+--       custom_tools = function()
+--         return {
+--           require("mcphub.extensions.avante").mcp_tool(),
+--         }
+--       end,
+--
+--   },
+--
+--   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+--   build = "make",
+--
+--   -- config = function()
+--   --   require("avante").setup({})
+--   -- end,
+--
+--   dependencies = {
+--     "nvim-treesitter/nvim-treesitter",
+--     "stevearc/dressing.nvim",
+--     "nvim-lua/plenary.nvim",
+--     "MunifTanjim/nui.nvim",
+--     --- The below dependencies are optional,
+--     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+--     "zbirenbaum/copilot.lua", -- for providers='copilot'
+--     {
+--       -- support for image pasting
+--       "HakonHarnes/img-clip.nvim",
+--       event = "VeryLazy",
+--       opts = {
+--         -- recommended settings
+--         default = {
+--           embed_image_as_base64 = false,
+--           prompt_for_file_name = false,
+--           drag_and_drop = {
+--             insert_mode = true,
+--           },
+--           -- required for Windows users
+--           use_absolute_path = true,
+--         },
+--       },
+--     },
+--     {
+--       -- Make sure to set this up properly if you have lazy=true
+--       'MeanderingProgrammer/render-markdown.nvim',
+--       opts = {
+--         file_types = { "markdown", "Avante" },
+--       },
+--       ft = { "markdown", "Avante" },
+--     },
+--   },
+-- }
