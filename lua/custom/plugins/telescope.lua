9f402c8ea93c8e45a7dd515ab030d1072a3d3439
Gambhir <gambhir786191ss@gmail.com>
2026-04-19 20:09:15 +0200

    chore: disable treesitter plug in telescope

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -49,11 +49,17 @@ return {
       -- You can put your default mappings / updates / etc. in here
       --  All the info you're looking for is in `:help telescope.setup()`
       --
-      -- defaults = {
+      defaults = {
+        preview = {
+          treesitter = {
+            enable = false
+          }
+        }
+
       --   mappings = {
       --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
       --   },
-      -- },
+      },
       -- pickers = {}
       extensions = {
         ['ui-select'] = {
